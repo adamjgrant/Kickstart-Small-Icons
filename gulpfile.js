7d9734c15d5ffc4c0fb3b6bcbc7c5c277d6193e4
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-10-24 16:46:58 -0400

    Adding example file

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,32 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   jade = require('gulp-jade');
 
+var icons = [
+  {
+    id: "backpack",
+    viewbox: "bar"
+  }
+]
+
 gulp.task('default', function() {
-  gulp.src(['*.jade'])
+  gulp.src(['*.svg.jade'])
     .pipe(jade({
       pretty: true,
       locals: {
         fs: require('fs'),
-        icons: [
-          {
-            id: "backpack",
-            viewbox: "bar"
-          }
-        ]
+        icons: icons
       }
     }))
     .pipe(rename('svg-defs.svg'))
+    .pipe(gulp.dest('./'))
+
+  gulp.src(['index.html.jade'])
+    .pipe(jade({
+      locals: {
+        icons: icons
+      },
+      pretty: true
+    }))
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('./'))
 });
