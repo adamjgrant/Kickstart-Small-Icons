5f4412e74c4f2ec222d45c11aba26eb327eb9c6d
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-10 16:04:27 -0500

    Baby steps with reading files

--- gulpfile.js
+++ gulpfile.js
@@ -1,145 +1,53 @@
 var gulp = require('gulp'),
   rename = require('gulp-rename'),
   sass = require('gulp-ruby-sass'),
-  jade = require('gulp-jade');
+  jade = require('gulp-jade'),
+  replace = require('gulp-replace'),
+  tap = require('gulp-tap')
+  path = require('path');
 
 var defaultViewBox = "0 0 32 32"
-var icons = [{
-  id: "backpack2"
-}, {
-  id: "backpack"
-}, {
-  id: "bill"
-}, {
-  id: "bookmark"
-}, {
-  id: "bookshelf"
-}, {
-  id: "briefcase"
-}, {
-  id: "bus"
-}, {
-  id: "calc"
-}, {
-  id: "candy"
-}, {
-  id: "car"
-}, {
-  id: "chalkboard"
-}, {
-  id: "clock"
-}, {
-  id: "cloud-check"
-}, {
-  id: "cloud-down"
-}, {
-  id: "cloud-error"
-}, {
-  id: "cloud-refresh"
-}, {
-  id: "cloud-up"
-}, {
-  id: "donut"
-}, {
-  id: "drop"
-}, {
-  id: "eye"
-}, {
-  id: "flag"
-}, {
-  id: "glasses"
-}, {
-  id: "glove"
-}, {
-  id: "hamburger"
-}, {
-  id: "hand"
-}, {
-  id: "hotdog"
-}, {
-  id: "knife"
-}, {
-  id: "label"
-}, {
-  id: "map"
-}, {
-  id: "map2"
-}, {
-  id: "marker"
-}, {
-  id: "mcfly"
-}, {
-  id: "medicine"
-}, {
-  id: "mountain"
-}, {
-  id: "muffin"
-}, {
-  id: "open-letter"
-}, {
-  id: "packman"
-}, {
-  id: "paper-plane"
-}, {
-  id: "photo 2"
-}, {
-  id: "piggy"
-}, {
-  id: "pin"
-}, {
-  id: "pizza"
-}, {
-  id: "r2d2"
-}, {
-  id: "rocket"
-}, {
-  id: "sale"
-}, {
-  id: "skull"
-}, {
-  id: "speakers"
-}, {
-  id: "store"
-}, {
-  id: "tactic"
-}, {
-  id: "toaster"
-}, {
-  id: "train"
-}, {
-  id: "umbrella"
-}, {
-  id: "watch"
-}, {
-  id: "www"
-}]
 
-gulp.task('default', function() {
-  gulp.src(['*.svg.jade'])
-    .pipe(jade({
-      pretty: true,
-      locals: {
-        fs: require('fs'),
-        icons: icons,
-        defaultViewBox: defaultViewBox
-      }
+gulp.task('default', ['responsive']);
+
+gulp.task('responsive', function() {
+  gulp.src("./svgs/**/*.svg")
+    .pipe(tap(function(file, t) {
+      // Get filenames, without paths.
+      console.log(path.basename(file.path, '.svg'))
+
+      // Get "size" directory.
+      console.log(path.dirname(path.relative('./svgs', file.path)))
     }))
-    .pipe(rename('svg-defs.svg'))
-    .pipe(gulp.dest('./public/'))
-
-  gulp.src(['index.html.jade'])
-    .pipe(jade({
-      locals: {
-        fs: require('fs'),
-        icons: icons,
-        defaultViewBox: defaultViewBox
-      },
-      pretty: true
-    }))
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('./public/'))
-
-  gulp.src(['icons.sass'])
-    .pipe(sass())
-    .pipe(gulp.dest('./public/'))
+    .pipe(gulp.dest('./public'));
 });
+
+// gulp.task('default', function() {
+//   gulp.src(['*.svg.jade'])
+//     .pipe(jade({
+//       pretty: true,
+//       locals: {
+//         fs: require('fs'),
+//         icons: icons,
+//         defaultViewBox: defaultViewBox
+//       }
+//     }))
+//     .pipe(rename('svg-defs.svg'))
+//     .pipe(gulp.dest('./public/'))
+//
+//   gulp.src(['index.html.jade'])
+//     .pipe(jade({
+//       locals: {
+//         fs: require('fs'),
+//         icons: icons,
+//         defaultViewBox: defaultViewBox
+//       },
+//       pretty: true
+//     }))
+//     .pipe(rename('index.html'))
+//     .pipe(gulp.dest('./public/'))
+//
+//   gulp.src(['icons.sass'])
+//     .pipe(sass())
+//     .pipe(gulp.dest('./public/'))
+// });
