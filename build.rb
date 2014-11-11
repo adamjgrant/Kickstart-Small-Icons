5e2c3ee8ddfaf78ad99061ea4b9f816b45318e33
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-11 18:05:11 -0500

    Adding more basic logic

--- build.rb
+++ build.rb
@@ -3,7 +3,30 @@
 require 'pathname'
 
 def build
-  puts "hello world"
+  createResponsiveSVG
+
+  # Debugging
+  @svg_names.each do |svg_name|
+    puts File.basename(svg_name, '.svg')
+  end
+end
+
+def createResponsiveSVG
+  @svg_names = Dir["./svgs/large/*.svg"]
+  File.open("./public/responsive.svg", "w") do |f|
+    f.truncate 0
+    f.puts "<svg style=\"none\">\n"
+    @svg_names.each do |svg_name|
+      f.puts "  <symbol>"
+      addPathsToFile(f)
+      f.puts "  </symbol>"
+    end
+    f.puts "</svg>\n"
+  end
+end
+
+def addPathsToFile(f, dir = nil, className = nil)
+  f.puts "    b\n"
 end
 
 build
