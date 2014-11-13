c992da025863756e4abab5ba2fd56d46ec71310e
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-13 18:05:22 -0500

    Strangely working in IE

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,21 +1,14 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     sass = require('gulp-ruby-sass'),
-    connect = require('gulp-connect'),
     fs = require('fs');
 
-gulp.task('default', ['compile'], function() {
-  connect.server({
-    root: 'public'
-  })
-});
-
-gulp.task('compile', function() {
+gulp.task('default', function() {
   gulp.src(['*.sass'])
     .pipe(sass())
     .pipe(gulp.dest('./public'))
 
-  gulp.src(['*.js', '!gulpfile.js'])
+  gulp.src(['*.js'])
     .pipe(gulp.dest('./public'))
 
   return gulp.src(['*.jade'])
